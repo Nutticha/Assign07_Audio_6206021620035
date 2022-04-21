78f8ec3bf8fce82c3cf76561a9d17ae78750d74d
Nutticha <nutticha30234@gmail.com>
2022-04-21 21:52:20 +0700

    Initial commit

---
+++ app/src/main/java/com/nutticha/assign07_audio_6206021620035/MainActivity.java
@@ -0,0 +1,21 @@
+package com.nutticha.assign07_audio_6206021620035;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+public class MainActivity extends AppCompatActivity
+    
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
